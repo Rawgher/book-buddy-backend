b8f6711ed36fb7ad56d6246a6fb2c9db830d364d
Rawgher <rdpender3@gmail.com>
2025-01-25 09:35:00 -0800

    tweaking files

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = process.env.SECRET_KEY;
 
-const PORT = +process.env.PORT || 5000;
+const PORT = +process.env.PORT;
 
 function getDatabaseUri() {
   const dbName = process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
@@ -11,13 +11,6 @@ function getDatabaseUri() {
 }
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
-console.log("Book Buddy Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
 
 module.exports = {
   SECRET_KEY,
